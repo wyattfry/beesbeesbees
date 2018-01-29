1fd26f8bf36cc5c3330445d6168761c4f28b67de
Wyatt Fry <wyattfry@gmail.com>
2018-01-28 22:49:56 -0800

    Passes all tests, having issues with Pomander eslint

--- src/Grub.js
+++ src/Grub.js
@@ -2,6 +2,5 @@ var Grub = function() {
   this.age = 0;
   this.color = 'pink';
   this.food = 'jelly';
-  this.eat = function() {};
 };
-Grub.prototype.constructor = Grub;
+Grub.prototype.eat = function() {};
